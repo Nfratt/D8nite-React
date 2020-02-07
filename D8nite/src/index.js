9f420b8aa4ccfe3ae8869b3a02bfae31cbf40d2a
nick Frattaroli <nafratt@gmail.com>
2020-02-07 16:18:10 -0500

    end of day

--- D8nite/src/index.js
+++ D8nite/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+import Search from "./search"
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Search />, document.getElementById('droot'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ D8nite/src/search.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./Components/Nav";
+import Logo from "./Components/Logo";
+import Restaurants from "./Components/Restaurants";
+import Events from "./Components/Events";
+import Movies from "./Components/Movies"
+import Forecast from "./Components/Forecast";
+
+function Search() {
+  return (
+    <div className="d8nite core">
+      <Navbar />
+      <br />
+      <div id="page results" className="container">
+        {/*     <section style="box-sizing: border-box; width:60%; float:left">  */}
+        <Logo />
+        <br />
+        <div id="results rest" className="row">
+          <Restaurants />
+        </div>
+        <br />
+        <div id="page resultsEvents" className="row">
+          <Events/>
+        </div>
+        <br/>
+        <div id="resultsMovies" className="row">
+          <Movies/>
+          </div>
+      </div>
+      <aside className="weather">
+        <Forecast/>
+      </aside>
+    </div>
+  );
+}
+
+export default Search;
